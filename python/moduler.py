0eae803c43435494dbce8a6f91082182ac3388ee
Jong Soo Lee <ljs8347625@naver.com>
2022-01-28 00:09:38 +0900

    모듈러 문제 수정

--- python/moduler.py
+++ python/moduler.py
@@ -3,15 +3,20 @@ def caesar(word, n):
     result=''
     for i in range(len(word)):
         data = ord(word[i])
-        index= data%122
+        index= (data+n)%123
+        # print(index)
         
         if index<65:
-            data_c = data+n+65
-            data_r = chr(data_c)
-            result = result + data_r
+            if data+n <97:
+                data_c = index+65
+                data_r = chr(data_c)
+                result = result + data_r
+            elif data+n >=97:
+                data_c=index+97
+                data_r = chr(data_c)
+                result = result + data_r
         elif index>=65:
-            data_c = data+n
-            data_r = chr(data_c)
+            data_r = chr(index)
             result = result + data_r
     return result
 
