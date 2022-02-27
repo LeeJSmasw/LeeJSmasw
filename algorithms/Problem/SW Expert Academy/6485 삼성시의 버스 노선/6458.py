f0efea83a497153380f924128ddd52bbdc48f810
Jong Soo Lee <ljs8347625@naver.com>
2022-02-27 23:52:07 +0900

    2022 02-27 study

--- algorithms/Problem/SW Expert Academy/6485 삼성시의 버스 노선/6458.py
+++ algorithms/Problem/SW Expert Academy/6485 삼성시의 버스 노선/6458.py
@@ -1,53 +1,53 @@
-'''
-삼성시에 있는 5,000개의 버스 정류장은 관리의 편의를 위해 1에서 5,000까지 번호가 붙어 있다.
-
-그리고 버스 노선은 N개가 있는데, i번째 버스 노선은 번호가 Ai이상이고,
-
-Bi이하인 모든 정류장만을 다니는 버스 노선이다.
-
-P개의 버스 정류장에 대해 각 정류장에 몇 개의 버스 노선이 다니는지 구하는 프로그램을 작성하라.
-
-
-[입력]
-
-첫 번째 줄에 테스트 케이스의 수 T가 주어진다.
-
-각 테스트 케이스의 첫 번째 줄에는 하나의 정수 N ( 1 ≤ N ≤ 500 )이 주어진다.
-
-다음 N개의 줄의 i번째 줄에는 두 정수 Ai, Bi ( 1 ≤ Ai ≤ Bi ≤ 5,000 )가 공백 하나로 구분되어 주어진다.
-
-다음 줄에는 하나의 정수 P ( 1 ≤ P ≤ 500 )가 주어진다.
-
-다음 P개의 줄의 j번째 줄에는 하나의 정수 Cj ( 1 ≤ Cj ≤ 5,000 ) 가 주어진다.
-
-
-[출력]
-
-각 테스트 케이스마다 ‘#x’(x는 테스트케이스 번호를 의미하며 1부터 시작한다)를 출력하고 한 칸을 띄운 후,
-
-한 줄에 P개의 정수를 공백 하나로 구분하여 출력한다.
-
-j번째 정수는 Cj번 버스 정류장을 지나는 버스 노선의 개수여야 한다.
-
-
-'''
-
 import sys
 sys.stdin = open('6458_input.txt','r')
-T = input()
+
+T = int(input())
 for tc in range(1,T+1):
-    N = int(input())
-    for _ in range(N):
-        ai, bi = map(input().split())
+    N = int(input()) # 입력 되는 케이스
+    bus_stop = [0] * 5001 # 1부터 5000까지 있으니까 0은 걍 있다고치고 1부터 5000까지 선언하기 위해 1을 더함 ㅎ
+    real_bus = [] # cj 정보를 받을 리스트임
+    result = '' #결과를 받을 리스트임
     
+    for _ in range(N):
+        ai, bi = map(int,input().split())
+        for i in (ai, bi+1):
+            bus_stop[i] += 1
+        
+
     P = int(input())
     
-    # Cj 이부분이 
+    
+
+    for c in range(P): # cj 정보를 받아보자
+        bus_use = int(input())
+        real_bus.append(bus_use)
+
+            
+    for k in real_bus:
         
+            result += str(bus_stop[k]) + ' '
+
+
+    print(f'#{tc} {result}')
     
     
-    
-    
-    
-    
-    print(f'#{tc} ')
+#천소희님의 코드
+tc = int(input())
+ 
+for t in range(1, tc + 1):
+    N = int(input())
+    bus_cnt = [0] * 5000
+ 
+    for i in range(N):
+        start, end = map(int, input().split())
+        for j in range(start - 1, end):
+            bus_cnt[j] += 1
+ 
+    P = int(input())
+ 
+    result = ''
+ 
+    for i in range(P):
+        result += str((bus_cnt[int(input())-1])) + ' '
+ 
+    print(f'#{t} {result}')
