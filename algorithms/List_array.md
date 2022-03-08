ff3cd916f15aa97d7c06a5ccdf884c486c77f029
Jong Soo Lee <ljs8347625@naver.com>
2022-03-08 23:13:19 +0900

    study

---
+++ algorithms/List_array.md
@@ -0,0 +1,61 @@
+# Numpy array와 Python list 차이
+
+출처 : https://bearwoong.tistory.com/60
+
+## 1. 연산
+
+```markdown
+# 1. numpy array는 numpy array끼리 연산이 가능하지만 Python list는 덧셈만 가능하다.
+# numpy 경우
+[10, 5, 3, 7, 1, 5] + [10, 5, 3, 7, 1, 5] = [20, 10, 6, 14, 2, 10]
+# list 경우
+[10, 5, 3, 7, 1, 5] + [10, 5, 3, 7, 1, 5] = [10, 5, 3, 7, 1, 5, 10, 5, 3, 7, 1, 5] 
+
+같은 맥락으로 numpy array는 numpy array 끼리 뺄셈, 곱셈, 나눗셈이 가능하다. 같은 인덱스에 있는 값들끼리 연산해준다. Pythom list는 리스트끼리 뺄셈, 곱셈, 나눗셈을 하면 오류가 난다.
+```
+
+
+
+```
+# 2.  numpy array는 array 전체에 연산이 가능하지만 Python list는 곱셈만 가능하다.
+
+# numpy array 경우 
+ [10, 5, 3, 7, 1, 5] + 5 = [15, 10, 8, 12, 6, 10]
+
+# list 경우
+ [10, 5, 3, 7, 1, 5] * 2 = [10, 5, 3, 7, 1, 5, 10, 5, 3, 7, 1, 5]
+```
+
+
+
+## 2. 성능차이
+
+```markdown
+- python list는 다양한 자료형이 저장가능 
+- numpy array는 같은 자료형만 넣을 수 있다. 
+
+numpy array가 python list에 비해 문법이 간단하고 성능이 뛰어나다.
+예를들어 1억개의 요소가 있는 기본 python list에 곱하기 2를 하려면 반복문을 1억번 돌려야 하는데 numpy array는 그냥 * 2 만 붙여주면 된다. 문법이 간단할 뿐만 아니라 실제 실행했을 때도 numpy array가 훨씬 짧은 시간에 연산을 마친다.
+
+```
+
+
+
+# 결론
+
+- python list는 값을 추가하고 제거에 사용,
+- numpy array는 수치 계산이 많고 복잡할 때나 행렬같은 다차원 배열
+
+상황에 맞게 사용하면 된다.
+
+
+
+```markdown
+pandas 의 DataFrame
+python list나 numpy array와 비슷하게 pandas DataFrame 도 여러 값들을 저장할 때 쓰인다.
+DateFrame 은 표형식의 데이터를 담기위한 자료형으로 2차원 numpy array에 부가적인 기능이 추가된 것이다. 
+(DataFrame이 numpy array를 기반으로 만들어져 있어서 pandas를 쓰려면 numpy가 먼저 설치되어 있어야한다.)
+
+예를들어 각 행과 열에 이름을 붙여줄 수 있어서 숫자인덱스가 아닌 정해준 이름으로 값을 찾을 수 있다. numpy array에서는 숫자 인덱스만 가능했다. 또 2차원 numpyarray에서는 모든 값이 같은 자료형이어야 하는 제약이 있었지만 DataFrame 에서는 다양한 자료형의 데이터를 담을 수 있다.
+```
+
--- algorithms/String/회문2/회문2.py
+++ algorithms/String/회문2/회문2.py
@@ -1,3 +1,114 @@
 import sys
 sys.stdin = open('회문2.py','r')
 
+for tc in range(1,11):
+    #가로, 세로 각각에 대해서 직선을만 판단한다
+    
+    
+    
+    print(f'#{tc} {result}')
+    
+# 성규님 코드    
+for t in range(10):
+    tc = int(input())
+    arr = [list(input()) for _ in range(100)]
+ 
+    max_cnt = 0
+    for i in range(98):
+        for j in range(98):
+            if arr[i][j] == arr[i][j+1] and arr[i][j-1] != arr[i][j]:
+                col_l = j
+                col_r = j+1
+                cnt = 0
+                while arr[i][col_l] == arr[i][col_r]:
+                    col_l -= 1
+                    col_r += 1
+                    cnt += 1
+                    if cnt * 2 >= max_cnt:
+                        max_cnt = cnt * 2
+                    if col_l == -1 or col_r == 100:
+                        break
+            else:
+                col_l = j - 1
+                col_r = j + 1
+                cnt = 0
+                while arr[i][col_l] == arr[i][col_r]:
+                    col_l -= 1
+                    col_r += 1
+                    cnt += 1
+                    if cnt * 2 + 1 >= max_cnt:
+                        max_cnt = cnt * 2 + 1
+                    if col_l == -1 or col_r == 100:
+                        break
+ 
+            if arr[j][i] == arr[j+1][i] and arr[j-1][i] != arr[j][i]:
+                col_l = j
+                col_r = j+1
+                cnt = 0
+                while arr[col_l][i] == arr[col_r][i]:
+                    col_l -= 1
+                    col_r += 1
+                    cnt += 1
+                    if cnt * 2 >= max_cnt:
+                        max_cnt = cnt * 2
+                    if col_l == -1 or col_r == 100:
+                        break
+            else:
+                col_l = j - 1
+                col_r = j + 1
+                cnt = 0
+                while arr[col_l][i] == arr[col_r][i]:
+                    col_l -= 1
+                    col_r += 1
+                    cnt += 1
+                    if cnt * 2 + 1 >= max_cnt:
+                        max_cnt = cnt * 2 + 1
+                    if col_l == 0 or col_r == 99:
+                        break
+ 
+    print(f'#{tc} {max_cnt}')
+    
+#소희님꺼
+
+# 회문인지 검사하는 함수
+def search(word):
+    length = len(word)
+    for i in range(length):
+        if word[i] != word[-1-i]:
+            return False
+    # 같다면 최대값을 반환한다
+    else:
+        if max_len > length:
+            return max_len
+        else:
+            return length
+ 
+for _ in range(10):
+    tc = input()
+ 
+    max_len = 1
+ 
+    string = [input() for _ in range(100)]
+ 
+    t_s = ''
+    t_string = []
+    for i in range(100):
+        for j in range(100):
+            t_s += string[j][i]
+        t_string.append(t_s)
+        t_s = ''
+ 
+    for i in range(100):
+        for j in range(100 - max_len):
+            # 제일 긴 것부터 돌리면서 회문이 맞다면 최대길이 갱신하고 넘긴다
+            for k in range(100, j + max_len -1 ,-1):
+                if search(string[i][j:k]):
+                    max_len = search(string[i][j:k])
+                    break
+ 
+                elif search(t_string[i][j:k]):
+                    max_len = search(t_string[i][j:k])
+                    break
+ 
+ 
+    print(f'#{tc} {max_len}')
