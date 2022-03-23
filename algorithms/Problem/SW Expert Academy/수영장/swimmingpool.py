8e506bf772e7fe01700cabb24ba567a457f4d465
Jong Soo Lee <ljs8347625@naver.com>
2022-03-23 21:09:48 +0900

    0323 수영장 풀어야 하는데 귀찮

--- algorithms/Problem/SW Expert Academy/수영장/swimmingpool.py
+++ algorithms/Problem/SW Expert Academy/수영장/swimmingpool.py
@@ -44,3 +44,58 @@ for tc in range(1, T + 1):
     result = min(result_case)
 
     print(f'#{tc} {result}')
+    
+    
+    '''
+    진하님 코드 오호 .. 
+    T = int(input())
+    for tc in range(1, 1 + T):
+    d, m, m3, y = map(int, input().split())
+    plan = [0]+list(map(int, input().split()))
+    result = [0]*13
+ 
+ 
+    for i in range(1,13):
+        tmp = [0,0,y,y]
+        # 1일 이용권 사용
+        tmp[0] = result[i-1] + plan[i]*d
+        # 1달 이용권 사용
+        tmp[1] = result[i-1] + m
+        # 3달 이용권 사용
+        if i>=3:
+            tmp[2] = result[i-3] + m3
+            #이게 왜 성립할까나 
+        result[i] = min(tmp)
+    print(f'#{tc} {result[12]}')
+    
+    
+    def cal(month, day):
+      global result
+    # print(month, day)
+ 
+    if month > 12:
+        if result > day:
+            result = day
+    else:
+        # 하루
+        cal(month + 1, day + plan[month]*fee[0])
+        # 한달
+        cal(month + 1, day + fee[1])
+        #세달
+        cal(month + 3, day + fee[2])
+ 
+    if result > fee[3]:
+        result = fee[3]
+    return
+ 
+T = int(input())
+ 
+for tc in range(1, T+1):
+    fee = list(map(int, input().split()))
+    plan = [0] + list(map(int, input().split()))
+    result = 99999999999
+ 
+    cal(0, 0)
+    print(f"#{tc} {result}")
+    
+    '''
