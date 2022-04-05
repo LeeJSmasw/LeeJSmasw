82f0492c200e78a150868c84c90af0f6590719ae
Jong Soo Lee <ljs8347625@naver.com>
2022-04-05 22:09:06 +0900

    0405

--- algorithms/Problem/SW Expert Academy/Othello/Othello.py
+++ algorithms/Problem/SW Expert Academy/Othello/Othello.py
@@ -1,42 +1,49 @@
 import sys
 sys.stdin = open('Othello.txt','r')
 
+#종훈님의 코드 // 분석해 보자 
+def over(a, b): 
+    if 0 <= a <= N and 0 <= b <= N:
+        return True
+    return False # a와 b가 모두 N, M 보다 클 때 게임이 끝남 // 와 이걸 놓쳤었네 >>  한 변 길이 N과 플레이어가 돌을 놓는 횟수 M이 주어진다. N은 4, 6, 8 중 하나이다.
 T = int(input())
 for t in range(1, T + 1):
-    N, M = map(int, input().split()) # 여기서 N과 M을 입력 받곱 
-    bode = [[0] * (N + 1) for _ in range(N + 1)] #왜 여기서 N+1 로 맵을 만들까?? # 0(N//2을 이용해 가운데에서 시작해, 0(첫 번째) 칸을 안쓰기 위해서인가)
-    start = N // 2 # 와 2분의 1일 
-    bode[start][start] = bode[start + 1][start + 1] = 2
-    bode[start + 1][start] = bode[start][start + 1] = 1
-    for m in range(M):
-        x, y, st = map(int, input().split())
-        bode[y][x] = st
-        for dy, dx in ((1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)): #
+    N, M = map(int, input().split()) # 여기서 N과 M을 입력 받곱, N : 멤의 크기, M 플레이어가 돌을 놓는 횟수 --> 한 번 input을 봐볼까 
+    map = [[0] * (N) for _ in range(N)]  # 0(N//2을 이용해 가운데에서 시작해, 0(첫 번째) 칸을 안쓰기 위해
+    first = N // 2 # 와 2분의 1일 
+    map[first][first] = map[first + 1][first + 1] = 2 # 이걸로 초반 백돌 설정, 파이써닉한 선언
+    map[first + 1][first] = map[first][first + 1] = 1 # 이걸로 초반 흑돌 설정 
+    for m in range(M): # m은 변수로 사용하고 M은 횟수를 의미함
+        x, y, data = map(int, input().split()) # x, y 
+        map[y][x] = data #받은걸로 하나 씩 표함
+        
+        #사실상 여기서부터가 진짜 중요한 것 같은데.. 음
+        for dy, dx in ((1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)): # 튜플 8개를 반복하는데, 그안에 0번째 -> dx, 1번쨰 ->dy이에 때려 박는다.
             revers = []
-            for k in range(1, N):
+            for k in range(1, N): #근데 0부터, N으로서 선언해서 안해도 된는거 아닌가??/ 근데 왜 N번 반복할까??????????????????
                 new_y = y + dy * k
                 new_x = x + dx * k
                 
+                #와 근데 이거 은근히 복잡하네,, 음 한 번 정리해보자 
                 if over(new_y, new_x):
-                    if bode[new_y][new_x] == 0:
+                    if map[new_y][new_x] == 0:
                         break
-                    elif bode[new_y][new_x] == st:
+                    elif map[new_y][new_x] == data:
                         for ry, rx in revers:
-                            bode[ry][rx] = st
+                            map[ry][rx] = data
                         break
                     else:
                         revers.append((new_y, new_x))
                 else:
                     break
-    wst = 0
-    bst = 0
-    for bd in bode:
-        wst += bd.count(2)
-        bst += bd.count(1)
-    print(f'#{t} {bst} {wst}')
+    white = 0
+    black = 0
+    for bd in map:
+        white += bd.count(2) #백돌 개수 새기
+        black += bd.count(1) #흑돌 개수 새기 오케이 
+    print(f'#{t} {black} {white}')
     
 
-print(f'{T} {}')
 
 '''
 종훈님의 코드 // 분석해 보자 
