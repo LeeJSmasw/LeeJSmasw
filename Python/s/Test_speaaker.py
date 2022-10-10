06eb76c3dad339cf8d316fd79b3c352d391c5bff
Jong Soo Lee <ljs8347625@naver.com>
2022-10-10 21:47:31 +0900

    come back 221010

---
+++ Python/s/Test_speaaker.py
@@ -0,0 +1,8 @@
+sa
+# python -env myenv ,, 스피커 해보기 
+# /* 1. 가상환경 + TTS
+# (윈도우)
+# python -m venv myenv # 가상환경만들기
+# .\myenv\Scripts\activate  #가상환경 활성화하기 
+# pip install gTTS #구글에서 제공하는 라이브러리 다운 
+# pip install playsound==1.2.2 # 
